fc8f364749bc9775c78a7e9f4c56322748c92eed
SfedBro <fegalinka@mail.ru>
2025-10-14 13:39:23 +0300

    Shards commit?

---
+++ Assets/Scripts/Shards/TestShardTypingScript.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class TestShardTypingScript : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+    public void OnHold()
+    {
+
+    }
+    public void Passive()
+    {
+
+    }
+    public void OnShoot()
+    {
+
+    }
+    public void OnTouch()
+    {
+
+    }
+    public void OnActivate()
+    {
+
+    }
+}
