6382dddb2e49eb620fe490f6387eb8da7e5ace7d
SfedBro <fegalinka@mail.ru>
2025-10-17 20:20:21 +0300

    Unified Melee Attack script

---
+++ Assets/Scripts/Player/Weapons/CloseCombatAttack/CloseCombatWeapon.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public abstract class CloseCombatWeapon : BaseWeapon
+{
+    [Header("Weapon arguments")]
+    public Collider2D attackCollider;
+    public string[] affectedMasks;
+    public float damage;
+    public float offset;
+    public float defaultCoolDown;
+
+    [Header("Private arguments")]
+    private float coolDown;
+    private ContactFilter2D _filter;
+    private readonly Collider2D[] _hitResults = new Collider2D[32];
+
+    void Awake()
+    {
+        _filter = new ContactFilter2D { useTriggers = true };
+        if (affectedMasks.Length > 0)
+            _filter.SetLayerMask(LayerMask.GetMask(affectedMasks));
+    }
+
+    private void Update()
+    {
+        if (coolDown > 0)
+        {
+            coolDown -= Time.deltaTime;
+            if (coolDown < 0) coolDown = 0;
+        }
+    }
+
+    public override void ShootWeapon(Transform player, Vector2 dir)
+    {
+        if (coolDown <= 0f)
+        {
+            if (!attackCollider)
+            {
+                Debug.LogWarning($"{name}: attackCollider not assigned!");
+                return;
+            }
+            int hitCount = Physics2D.OverlapCollider(attackCollider, _filter, _hitResults);
+            if (hitCount > 0)
+                MeleeAttack(_hitResults, hitCount);
+            coolDown = defaultCoolDown;
+        }
+    }
+
+    protected abstract void MeleeAttack(Collider2D[] targets, int targetsCount);
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackCollider)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireCube(attackCollider.bounds.center, attackCollider.bounds.size);
+        }
+    }
+}
